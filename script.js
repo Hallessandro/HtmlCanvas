93f27073214e8c8f8e589fa9869f028bea819b23
Hallessandro <hallessandro.dvilla@esig.com.br>
2019-11-13 17:44:05 -0300

    Add img de background

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ window.addEventListener("load", () => {
     const canvas = document.querySelector("#canvas");
     const ctx = canvas.getContext("2d");
 
-    canvas.height = window.innerHeight; 
-    canvas.width = window.innerWidth; 
+    canvas.height = 480; 
+    canvas.width = 640; 
 
     let painting = false; 
+
+    let img = document.createElement("img");
+    img.src = "./mononokeHime.jpg";
+    img.addEventListener("load", () => {
+        ctx.drawImage(img, 10, 10, 640, 480);
+    });
 
     function startPosition(e){
         painting = true; 
